9d9b5108a7c82290f5302929677801aa29529c72
rossg2021 <77760650+rossg2021@users.noreply.github.com>
2021-02-24 20:55:14 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const pI = 3.14;
+PI = 42;
+
+
+
+
+
+var can be redeclared and updated within the same scope with out an error 
+var variables are hoisted first in their scope 
+
+
+
+let cannot be redeclare a variable that has been already declared,
+ but can reasign a variable. additonally let is block scoped let variables are also hoisted to 
+ first.
+
+ const cannot redeclare a variable or reasign a varaible. they are also block scoped
+ and are also hoisted to the first of their scope.
+
